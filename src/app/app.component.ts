72b8aed218efa821d0cd114e708716705b5cc5e3
Peter Grman <peter.grman@gmail.com>
2017-06-23 17:54:10 +0200

    add sample.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import sampleRaw from './sample.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  sample = sampleRaw;
+}
+
+if (sampleRaw === undefined) {
+  throw Error('sample raw is undefined');
+} else {
+  console.log(sampleRaw);
 }
